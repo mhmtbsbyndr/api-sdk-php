7378ae51a97654f195ef572ccea3f9e6b1af8239
Mehmet Basbayandur <mhmtbsbyndr@hotmail.com>
2022-10-01 14:08:10 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
-Test api-sdk for php-client
-
-<a href="https://bit.ly/3UN6FhL"> Garpa.de/gartenmoebel </a>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Test api-sdk for php-client</p>'; ?> 
+   <a href="https://bit.ly/3UN6FhL"> Garpa.de/gartenmoebel </a>
+ </body>
+</html>
